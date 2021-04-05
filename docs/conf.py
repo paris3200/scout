c0d941b8a5484df97cacc4c10d973e2bf41351bc
Jason Paris <paris3200@gmail.com>
2021-04-05 14:25:47 -0400

    Add sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,6 @@
+"""Sphinx configuration."""
+from datetime import datetime
+
+project = "Scout"
+author = "Jason Paris "
+copyright = f"{datetime.now().year}, {author}"
--- noxfile.py
+++ noxfile.py
@@ -5,8 +5,9 @@ import nox
 from nox_poetry import session
 from nox_poetry.sessions import Session
 
-locations = "src", "tests", "noxfile.py"
+locations = "docs/conf.py", "src", "tests", "noxfile.py"
 nox.options.sessions = "lint", "mypy", "pytype", "safety", "tests"
+package = "scout"
 
 
 @session(python=["3.7", "3.8", "3.9"])
@@ -71,3 +72,19 @@ def pytype(session: Session) -> None:
     args = session.posargs or ["--disable=import-error", *locations]
     session.install("pytype")
     session.run("pytype", *args)
+
+
+@session(python=["3.9", "3.8"])
+def xdoctest(session: Session) -> None:
+    """Run examples with xdoctest."""
+    args = session.posargs or ["all"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    session.install("xdoctest")
+    session.run("python", "-m", "xdoctest", package, *args)
+
+
+@session(python="3.9")
+def docs(session: Session) -> None:
+    """Build the documentation."""
+    session.install("sphinx")
+    session.run("sphinx-build", "docs", "docs/_build")
