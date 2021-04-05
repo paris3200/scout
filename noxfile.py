c91a77e1e07461db8e643a149cb3d606338ee621
Jason Paris <paris3200@gmail.com>
2021-04-04 21:11:56 -0400

    Add nox session tests

---
+++ noxfile.py
@@ -0,0 +1,10 @@
+"""Nox Sessions."""
+from nox_poetry import session
+from nox_poetry.sessions import Session
+
+
+@session(python=["3.7", "3.8", "3.9"])
+def tests(session: Session) -> None:
+    """Run the test suite."""
+    session.install("pytest", ".")
+    session.run("pytest")
