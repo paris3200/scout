6346a4e9e3e739fd6e1c2e0f3cc6750f1965dc2f
Jason Paris <paris3200@gmail.com>
2021-04-04 21:49:25 -0400

    Add lint session

--- noxfile.py
+++ noxfile.py
@@ -3,8 +3,19 @@ from nox_poetry import session
 from nox_poetry.sessions import Session
 
 
+locations = "src", "tests", "noxfile.py"
+
+
 @session(python=["3.7", "3.8", "3.9"])
 def tests(session: Session) -> None:
     """Run the test suite."""
     session.install("pytest", "pytest-cov", ".")
     session.run("pytest", "--cov")
+
+
+@session(python=["3.8", "3.9"])
+def lint(session: Session) -> None:
+    """Run the lint session."""
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
