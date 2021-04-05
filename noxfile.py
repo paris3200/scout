bc170c64039d9831109c8d06a71f7ebe6ecceef1
Jason Paris <paris3200@gmail.com>
2021-04-04 22:07:27 -0400

    Add flake8-bugbear

--- noxfile.py
+++ noxfile.py
@@ -18,7 +18,7 @@ def tests(session: Session) -> None:
 def lint(session: Session) -> None:
     """Run the lint session."""
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-isort")
+    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-isort")
     session.run("flake8", *args)
 
 
